800c88bbf80bcdc71e10d455a8c58ffde090edfc
Hassan Ahmed <alshabah4747@gmail.com>
2024-03-08 02:35:54 +0200

    Adding I am Hassan

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello from CI/CD!');
+  res.end('Hello from CI/CD, I am Hassan!');
 });
 
 server.listen(3000, () => {
