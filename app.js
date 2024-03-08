beb20cee8eae9887c27bd82375a54a55b7bb4961
Hassan Ahmed <alshabah4747@gmail.com>
2024-03-08 02:27:36 +0200

    Adding js code

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hello from CI/CD!');
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
